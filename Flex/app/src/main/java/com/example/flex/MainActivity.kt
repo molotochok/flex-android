6e05951d3b520a3e118ab21c991184e4b715bec0
Markiian Karpa <markian98@gmail.com>
2019-03-17 19:10:28 +0200

    Replaced PermissionActivity with PermissionService

--- Flex/app/src/main/java/com/example/flex/MainActivity.kt
+++ Flex/app/src/main/java/com/example/flex/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.flex
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlin.reflect.KClass
 
 class MainActivity : AppCompatActivity() {
+    private val permissionService = PermissionService(this, Manifest.permission.CAMERA)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun openScanReader(view: View){
-        val intent = Intent(this, ScanActivity::class.java)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
+        for(p in permissions){
+            when(p){
+                Manifest.permission.CAMERA ->{
+                    if(permissionService.checkPermission(p))
+                        openActivity(ScanActivity::class)
+                }
+            }
+        }
+    }
+    fun openScanReader(view: View){
+        if (permissionService.checkAndRequestPermissions()){
+            openActivity(ScanActivity::class)
+        }
+    }
+
+    private fun<T> openActivity(activityClass: KClass<T>) where T: Activity {
+        val intent = Intent(this, activityClass.java)
         startActivity(intent)
     }
 }
