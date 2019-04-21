9e17697b998878898c44ea471d41bddf508e2593
Markiian Karpa <markian98@gmail.com>
2019-04-21 16:59:58 +0300

    Added bottom navigation view

---
+++ Flex/app/src/main/java/com/example/flex/FirstActivity.kt
@@ -0,0 +1,51 @@
+package com.example.flex
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.flex.Services.PermissionService
+import kotlin.reflect.KClass
+
+class FirstActivity : AppCompatActivity() {
+    private val permissionService = PermissionService(this, Manifest.permission.CAMERA)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        for(p in permissions){
+            when(p){
+                Manifest.permission.CAMERA ->{
+                    if(permissionService.checkPermission(p))
+                        openActivity(ScanActivity::class)
+                }
+            }
+        }
+    }
+
+    // public methods
+    fun openScanReader(view: View){
+        if (permissionService.checkAndRequestPermissions()){
+            openActivity(ScanActivity::class)
+        }
+    }
+
+    // TODO: REMOVE
+    fun openVideoPlayer(videe: View) {
+        openActivity(VideoActivity::class)
+    }
+
+
+    // private methods
+    private fun<T> openActivity(activityClass: KClass<T>) where T: Activity {
+        val intent = Intent(this, activityClass.java)
+        startActivity(intent)
+    }
+}
--- Flex/app/src/main/java/com/example/flex/MainActivity.kt
+++ Flex/app/src/main/java/com/example/flex/MainActivity.kt
@@ -1,51 +1,34 @@
 package com.example.flex
 
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.example.flex.Services.PermissionService
-import kotlin.reflect.KClass
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val permissionService = PermissionService(this, Manifest.permission.CAMERA)
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_library)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_downloads)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_settings)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        for(p in permissions){
-            when(p){
-                Manifest.permission.CAMERA ->{
-                    if(permissionService.checkPermission(p))
-                        openActivity(ScanActivity::class)
-                }
-            }
-        }
-    }
-
-    // public methods
-    fun openScanReader(view: View){
-        if (permissionService.checkAndRequestPermissions()){
-            openActivity(ScanActivity::class)
-        }
-    }
-
-    // TODO: REMOVE
-    fun openVideoPlayer(videe: View) {
-        openActivity(VideoActivity::class)
-    }
-
-
-    // private methods
-    private fun<T> openActivity(activityClass: KClass<T>) where T: Activity {
-        val intent = Intent(this, activityClass.java)
-        startActivity(intent)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
