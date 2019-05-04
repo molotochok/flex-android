bf7b4a86509f0d128980ecc2274facf2e794f77f
Markiian Karpa <markian98@gmail.com>
2019-05-04 22:35:28 +0300

    Changed icons

--- Flex/app/src/main/java/com/example/flex/MainActivity.kt
+++ Flex/app/src/main/java/com/example/flex/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.flex
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
